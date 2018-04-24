4a9035bb9e0c5c8be67bb0b87dc2687e097ebc5d
Thomas <introt@gmail.com>
2018-04-24 18:34:46 +0200

    use apscheduler

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.base import BaseScheduler
+
 import telegramclient as tc
 import updates_dispatcher as ud
 import newsletter_service as nls
 import logging as log
-import schedule
 import scraper
-import time
 
 logFormatter = log.Formatter("%(asctime)s [%(levelname)-5.5s] %(message)s")
 
@@ -23,6 +24,7 @@ def configure_logger():
     rootLogger.setLevel("DEBUG")
 
 
+@BaseScheduler.scheduled_job('cron', minute='*/10')
 def fetch_updates():
     updates = tc.get_updates()
     log.info("Fetching updates...")
@@ -33,6 +35,11 @@ def fetch_updates():
         tc.set_offset(id + 1)
 
 
+@BaseScheduler.scheduled_job('cron',
+                             day_of_week='4-5',
+                             hour='7-19',
+                             minute=0,
+                             second=0)
 def fetch_nl():
     newsletters = scraper.scrape_nl()
     nls.check_and_notify(newsletters)
@@ -40,8 +47,5 @@ def fetch_nl():
 
 if __name__ == '__main__':
     configure_logger()
-    schedule.every(10).seconds.do(fetch_updates)
-    schedule.every(10).seconds.do(fetch_nl)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    scheduler = BackgroundScheduler()
+    scheduler.start()
