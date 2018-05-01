0c556be166417ba8de2b52133facbeaf04c2caa6
thomas <introt@gmail.com>
2018-05-01 15:14:13 +0200

    add type hint, convert cursor in dict

--- newsletter_manager.py
+++ newsletter_manager.py
@@ -1,19 +1,21 @@
 from pymongo import MongoClient
 from dateutil import parser
+from typing import Dict, List
 
 client = MongoClient()
 db = client['fugnolibot']
 
 
-def get_newsletters():
+def get_newsletters() -> List[Dict]:
     return list(db['newsletters'].find())
 
 
-def get_last_newsletter():
-    return db['newsletters'].find().sort('ts', -1).limit(1)
+def get_last_newsletter() -> Dict:
+    c = db['newsletters'].find().sort('ts', -1).limit(1)
+    return c[0] if c.count() == 1 else None
 
 
-def insert_newsletter(date, title, link):
+def insert_newsletter(date: str, title: str, link: str) -> None:
     ts = int(parser.parse(date).timestamp())
     db['newsletters'].insert_one({
         'date': date,
