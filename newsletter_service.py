9ee4144e49e1f9c2babdae3c5882fb5eb0c95a74
thomas <introt@gmail.com>
2018-05-01 15:42:01 +0200

    use week nr to check if there's a new newsletter to be notified

--- newsletter_service.py
+++ newsletter_service.py
@@ -9,10 +9,10 @@ from dateutil import parser
 
 def check_and_notify(nls):
     last_nl = max(nls, key=lambda nl: parser.parse(nl['added_on']))
-    added_on = parser.parse(last_nl['added_on']).date()
-    today = datetime.now().date()
-    if added_on == today:
-        log.info("Newsletter added today!")
+    added_on_week_nr = parser.parse(last_nl['added_on']).date().isocalendar()[1]
+    today_week_nr = datetime.now().date().isocalendar()[1]
+    if added_on_week_nr == today_week_nr:
+        log.info("Newsletter this week! week number: %s" % today_week_nr)
         nlm.insert_newsletter(
             last_nl['added_on'],
             last_nl['title'],
@@ -26,17 +26,17 @@ def check_and_notify(nls):
                 user_id = sub['user_id']
                 chat_type = sub['chat_type']
                 last_nl_notified = parse_last_nl_notified(sub)
-                if last_nl_notified and added_on == last_nl_notified:
+                if last_nl_notified and added_on_week_nr == last_nl_notified:
                     log.info("Sub was already notified, skipping")
                 else:
                     send_newsletter_message(last_nl, user_id)
                     sm.update_last_nl_notified(
                         chat_type,
                         user_id,
-                        str(today)
+                        str(today_week_nr)
                     )
     else:
-        log.info("Last nl was released on %s, skipping" % added_on)
+        log.info("Last nl was released on %s, skipping" % last_nl['added_on'])
 
 
 def send_newsletter_message(nl, user_id):
